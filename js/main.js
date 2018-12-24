78d294610f34f2b565f6744de459a6d5b4145617
alom <alom310@gmail.com>
2018-12-24 11:28:57 -0800

    created game logic

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var cardOne = "King";
+var cardTwo = "Queen";
+var cardThree = "Jack";
+var cardFour = "Ace";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
