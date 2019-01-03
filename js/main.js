4b83bfa87e467b7ce5ad8d8bf316b7b05340d852
alom <alom310@gmail.com>
2019-01-02 17:17:55 -0800

    created game functions

--- js/main.js
+++ js/main.js
@@ -1,22 +1,40 @@
 console.log("Up and running!");
-/*console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);*/
+
+
 
 var cards = ["queen", "queen","king","king"]
 
 var cardsInPlay = [];
 
-var cardOne= cards[0];
-cardsInPlay.push('cardOne');
-console.log("User flipped queen");
+var checkForMatch = function(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");
+}
+};
 
-var cardTwo= cards[2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped king");
+var flipCard = function(cardId){	
+checkForMatch();
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push('cards[cardId]');
+};
+flipCard(0);
+flipCard(2);
 
+
+//from slide 7 of unit 9 
+/* if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+
+**my original **
 if (cardsInPlay.length === 2) {
 	alert("Sorry, try again!");
 }
 	else if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	}
+*/
